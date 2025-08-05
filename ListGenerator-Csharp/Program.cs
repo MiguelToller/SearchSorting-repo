f46eedaf066890e9c7c212bc883c20f0a124a530
= <luizmigueltoller@gmail.com>
2025-08-05 08:21:40 -0300

    Update ListGenerator-Csharp

--- ListGenerator-Csharp/Program.cs
+++ ListGenerator-Csharp/Program.cs
@@ -7,11 +7,21 @@
             List<int> listaNumeros = new List<int>();
             List<string> listaPalavras = new List<string>();
 
+            /*
             Util.PopularAleatorioNumeros(listaNumeros, 10, 0, 100);
             Util.PopularAleatorioPalavras(listaPalavras, 5, 5);
 
             Util.ExibirLista(listaNumeros);
             Util.ExibirLista(listaPalavras);
+            */
+
+            string caminhoArquivo = "arquivo.txt";
+
+            Util.PopularNumeroDeArquivo(caminhoArquivo, 5, 0, 10);
+            Util.LerArquivo(caminhoArquivo);
+
+            Util.PopularPalavrasDeArquivo(caminhoArquivo, 5, 5);
+            Util.LerArquivo(caminhoArquivo);
         }
     }
 }
